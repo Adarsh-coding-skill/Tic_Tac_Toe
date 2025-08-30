72aca9f346a8d14d2ef11a7b7662d1cd3c19a179
Adarsh-coding-skill <adarshpandey1164809@gmail.com>
2025-08-30 20:31:23 +0530

    C++

--- Tic_Tac_toe.cpp
+++ Tic_Tac_toe.cpp
@@ -1,1 +1,57 @@
 #include <iostream>
+class TicTacToe
+{
+private:
+    char board[3][3];
+    char current_marker;
+    int current_player;
+
+public:
+    TicTacToe()
+    {
+        char count = '1';
+        for (int i = 1; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                board[i][j] = count++;
+            }
+        }
+
+        current_marker = 1;
+        current_player = 'X';
+    }
+
+    void drawBoard(){
+        cout<<"\n";
+        for(int i = 0; i< 3; i++){
+            for(int j =0; j< 3; j++0){
+                cout<<" "<<board[i][j];
+                if(j<2) cout <<"|";
+            }
+cout<<"\n";
+if(i<2) cout<<"--------\n";
+
+        }
+        cout<<"\n";
+    }
+
+
+    bool placeMarker(int slot){
+        int row = (slot-1)/3;
+        int col = (slot-1)%3;
+
+        if(slot < 1 || slot > 9) return false;
+
+        
+    }
+
+
+};
+
+
+int main(){
+
+
+    return 0;
+}
