81cf3806f06eb9aca4d769dba94734bc59770efd
Adarsh-coding-skill <adarshpandey1164809@gmail.com>
2025-08-30 21:14:10 +0530

    C++

--- Tic_Tac_toe.cpp
+++ Tic_Tac_toe.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+using namespace std;
+
 class TicTacToe
 {
 private:
@@ -10,7 +12,7 @@ public:
     TicTacToe()
     {
         char count = '1';
-        for (int i = 1; i < 3; i++)
+        for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
@@ -23,9 +25,9 @@ public:
     }
 
     void drawBoard(){
-        cout<<"\n";
+         cout<<"\n";
         for(int i = 0; i< 3; i++){
-            for(int j =0; j< 3; j++0){
+            for(int j =0; j<3; j++){
                 cout<<" "<<board[i][j];
                 if(j<2) cout <<"|";
             }
@@ -43,15 +45,81 @@ if(i<2) cout<<"--------\n";
 
         if(slot < 1 || slot > 9) return false;
 
-        
+        if(board[row][col] != 'X' && board[row][col] != 'O'){
+board[row][col]= current_marker;
+return true;
+        }
+
+        return false;
     }
 
+
+int checkWinner(){
+    for(int i =0; i < 3; i++){
+        if(board[i][0]==board[i][1] && board[i][1] == board[i][2]) return current_player;
+        if(board[0][i] == board[1][i] && board[1][i]== board[2][i]) return current_player;
+    }
+    if(board[0][0]==board[1][1] && board[1][1] == board[2][2]) return current_player;
+        if(board[0][2] == board[1][1] && board[1][1]== board[2][0]) return current_player;
+
+        return 0;
+}
+
+void swapPlayerAndMarker(){
+
+    if(current_marker =='X')current_marker ='O';
+    else current_marker ='X';
+
+    if(current_player == 1) current_player=2;
+    else current_player = 1;
+}
+
+void playGame(){
+    cout<<"Player 1, Choose your marker (X or O): ";
+    char marker_p1;
+    cin>>marker_p1;
+
+    current_player =1;
+    current_marker = marker_p1;
+
+    drawBoard();
+    int player_won;
+
+    for(int i =0;i<9; i++){
+        cout<<"Player "<< current_player<< ", enter your slot (1-9): ";
+        int slot;
+        cin>>slot;
+        if(!placeMarker(slot)){
+
+            cout<<"Invaild move! Try again.\n";
+            i--;
+            continue;
+        }
+
+        drawBoard();
+
+        player_won = checkWinner();
+        if(player_won==1){
+            cout<<"Player 1 wins!"<<endl;
+            return;
+        }
+        if(player_won==2){
+            cout<<"Player 2 wins!"<<endl;
+            return;
+        }
+        swapPlayerAndMarker();
+    }
+ cout << "It's a draw!"<<endl;
+
+}
 
 };
 
 
 int main(){
+TicTacToe game;
+game.playGame();
+return 0;
 
-
-    return 0;
+   
 }
